3a25e8871d017223cda5b21a1ba00694b9b021f8
Ziv Dunkelman <ziv.dunkelman@nextsilicon.com>
2024-05-12 14:17:34 +0000

    Nextsilicon implement RangePolicy Reduce using nsapi
    Allows for custom reducers to be supported with nextsilicon backend
    
    Signed-off-by: Ziv Dunkelman <ziv.dunkelman@nextsilicon.com>

--- core/src/NextSilicon/Kokkos_NextSilicon_ParallelReduce_Range.hpp
+++ core/src/NextSilicon/Kokkos_NextSilicon_ParallelReduce_Range.hpp
@@ -21,8 +21,11 @@
 #include <NextSilicon/Kokkos_NextSilicon_Macros.hpp>
 #include <NextSilicon/Kokkos_NextSilicon_FunctorAdapter.hpp>
 #include <NextSilicon/Kokkos_NextSilicon_ScheduleType.hpp>
+#include <nsapi/intrinsics.h>
+#include <nsapi/parallelism.h>
 #include <Kokkos_Parallel.hpp>
 #include <type_traits>
+#include <cstdint>
 
 namespace Kokkos::Experimental::Impl {
 
@@ -44,9 +47,14 @@ template <class CombinedFunctorReducerType, class... Traits>
 class Kokkos::Impl::ParallelReduce<CombinedFunctorReducerType,
                                    Kokkos::RangePolicy<Traits...>,
                                    Kokkos::Experimental::NextSilicon> {
-  using Policy      = RangePolicy<Traits...>;
-  using FunctorType = typename CombinedFunctorReducerType::functor_type;
-  using ReducerType = typename CombinedFunctorReducerType::reducer_type;
+  using ExecSpace    = Kokkos::Experimental::NextSilicon;
+  using memory_space = typename ExecSpace::memory_space;
+  using Policy       = RangePolicy<Traits...>;
+  using WorkTag      = typename Policy::work_tag;
+  using WorkRange    = typename Policy::WorkRange;
+  using Member       = typename Policy::member_type;
+  using FunctorType  = typename CombinedFunctorReducerType::functor_type;
+  using ReducerType  = typename CombinedFunctorReducerType::reducer_type;
 
   using Pointer   = typename ReducerType::pointer_type;
   using ValueType = typename ReducerType::value_type;
@@ -67,104 +75,91 @@ class Kokkos::Impl::ParallelReduce<CombinedFunctorReducerType,
         // access NextSiliconSpace
         m_result_ptr_on_device(false) {}
 
-  void execute() const {
+#pragma ns mark boundary
+  __attribute__((noinline)) void execute() const {
     auto const begin = m_policy.begin();
     auto const end   = m_policy.end();
 
-    // FIXME_NEXTSILICON: this should all happen on the device
-    ValueType val;
-    ReducerType const& reducer = m_functor_reducer.get_reducer();
-    reducer.init(&val);
+    auto const& functor = m_functor_reducer.get_functor();
+    auto const& reducer = m_functor_reducer.get_reducer();
 
     if (end <= begin) {
-      if (m_result_ptr_on_device) {
-        // FIXME_NEXTSILICON memcpy val to device
-        *m_result_ptr = val;
-      } else {
-        *m_result_ptr = val;
-      }
+      ValueType val;
+      reducer.init(&val);
+      *m_result_ptr = val;
       return;
     }
 
-    // ReducerType always has value_type.
-    // ReducerType is a FunctorAnalysis object that might either wrap the
-    // original functor (if the result type is a View or scalar) or the reducer
-    // (if the result type is a reducer). If the result type is not a reducer,
-    // we effectively do a sum reduction (but not using Kokkos::Sum, at least in
-    // the generic implementation). It's fine to use Kokkos::Sum, though, if you
-    // can't use the FunctorAnalysis object as the reducer.
-    Kokkos::Experimental::Impl::NextSiliconParallelReduceHelper(
-        m_functor_reducer.get_functor(),
-        std::conditional_t<
-            std::is_same_v<FunctorType, typename ReducerType::functor_type>,
-            Sum<ValueType>, typename ReducerType::functor_type>(val),
-        m_policy);
+    if (__nsapi_is_on_cg()) {
+      const auto num_iterations = end - begin;
+      uint32_t team_size        = nsapi_team_get_optimal_size(
+          reinterpret_cast<void*>(microtask), num_iterations);
 
-    // FIXME_NEXTSILICON: this should all happen on the device
-    reducer.final(&val);
+      // TODO: Cache this allocation in the instance
+      ValueType* values = reinterpret_cast<ValueType*>(
+          memory_space().allocate(team_size * sizeof(ValueType)));
 
-    // val is on the device,
-    if (m_result_ptr_on_device) {
-      *m_result_ptr = val;
-      // FIXME_NEXTSILICON sync? device has made result available
+      uint32_t* counter = reinterpret_cast<uint32_t*>(
+          memory_space().allocate(sizeof(uint32_t)));
+      // TODO: When moved to fixed allocation in instance, memset once.
+      *counter = 0;
+
+      nsapi_team_spawn(
+          reinterpret_cast<void*>(microtask), team_size,
+          /* memory_size */ 0,
+          nsapi_team_dimensions{static_cast<uint64_t>(num_iterations)},
+          &functor, &reducer, &m_policy, values, counter, m_result_ptr);
+
+      // TODO: When moving to instance memory, don't deallocate
+      memory_space().deallocate(values, team_size * sizeof(ValueType));
+      memory_space().deallocate(counter, sizeof(*counter));
+
+      // TODO: Fence if !m_result_ptr_on_device - not needed for now as
+      // nsapi_team_spawn is always sync.
     } else {
-      // FIXME_NEXTSILICON sync: wait for device to finish
-      *m_result_ptr = val;  // FIXME_NEXTSILICON memcpy val from device
+      ValueType value;
+      uint32_t counter = 0;
+      microtask(&functor, &reducer, &m_policy, &value, &counter, m_result_ptr);
     }
   }
-};
 
-#define KOKKOS_IMPL_NEXTSILICON_PARALLEL_REDUCE_HELPER(REDUCER, OPERATOR)      \
-  namespace Kokkos::Experimental::Impl {                                       \
-  template <class IndexType, class ValueType, class Functor>                   \
-  void NextSiliconParallelReduce##REDUCER(IndexType begin, IndexType end,      \
-                                          ValueType& aval,                     \
-                                          Functor const& functor) {            \
-    auto val = aval;                                                           \
-    KOKKOS_IMPL_NS_PRAGMA(omp parallel for reduction(OPERATOR:val))            \
-    for (auto i = begin; i < end; i++) {                                       \
-      functor(i, val);                                                         \
-    }                                                                          \
-    aval = val;                                                                \
-  }                                                                            \
-  } /* namespace Kokkos::Experimental::Impl */                                 \
-  template <class Functor, class Scalar, class Space, class... Traits>         \
-  struct Kokkos::Experimental::Impl::NextSiliconParallelReduceHelper<          \
-      Functor, Kokkos::REDUCER<Scalar, Space>, Kokkos::RangePolicy<Traits...>, \
-      true> {                                                                  \
-    using Policy    = RangePolicy<Traits...>;                                  \
-    using Reducer   = REDUCER<Scalar, Space>;                                  \
-    using ValueType = typename Reducer::value_type;                            \
-                                                                               \
-    NextSiliconParallelReduceHelper(Functor const& functor,                    \
-                                    Reducer const& reducer,                    \
-                                    Policy const& policy) {                    \
-      auto const begin = policy.begin();                                       \
-      auto const end   = policy.end();                                         \
-                                                                               \
-      if (end <= begin) {                                                      \
-        return;                                                                \
-      }                                                                        \
-                                                                               \
-      ValueType val;                                                           \
-      reducer.init(val);                                                       \
-                                                                               \
-      NextSiliconParallelReduce##REDUCER(begin, end, val, functor);            \
-                                                                               \
-      reducer.reference() = val;                                               \
-    }                                                                          \
+ private:
+  static void microtask(const FunctorType* functor, const ReducerType* reducer,
+                        const Policy* policy, ValueType* result_arr,
+                        uint32_t* counter, ValueType* result_ptr) {
+    uint32_t thread_index = nsapi_team_get_thread_index();
+    uint32_t team_size    = nsapi_team_get_team_size();
+
+    WorkRange range(*policy, thread_index, team_size);
+    const auto ibeg = range.begin();
+    const auto iend = range.end();
+
+    ValueType val;
+    reducer->init(&val);
+
+    for (auto i = ibeg; i < iend; ++i) {
+      (*functor)(i, val);
+    }
+
+    result_arr[thread_index] = val;
+
+    uint32_t counter_val = __atomic_add_fetch(counter, 1, __ATOMIC_SEQ_CST);
+    if (counter_val < team_size) {
+      return;
+    }
+
+    // Last thread to contribute reduction value (i.e counter got to team_size)
+    *counter = 0;
+
+    // Runs on a single thread, so running on RISC where single thread
+    // performance is better
+#pragma ns location risc
+    for (uint32_t i = 1; i < team_size; ++i) {
+      reducer->join(&result_arr[0], &result_arr[i]);
+    }
+    reducer->final(result_ptr);
+    *result_ptr = result_arr[0];
   }
-
-// FIXME_NEXTSILICON: workaround for OpenMP not supporting custom reductions
-KOKKOS_IMPL_NEXTSILICON_PARALLEL_REDUCE_HELPER(Sum, +);
-KOKKOS_IMPL_NEXTSILICON_PARALLEL_REDUCE_HELPER(Prod, *);
-KOKKOS_IMPL_NEXTSILICON_PARALLEL_REDUCE_HELPER(Min, min);
-KOKKOS_IMPL_NEXTSILICON_PARALLEL_REDUCE_HELPER(Max, max);
-KOKKOS_IMPL_NEXTSILICON_PARALLEL_REDUCE_HELPER(LAnd, &&);
-KOKKOS_IMPL_NEXTSILICON_PARALLEL_REDUCE_HELPER(LOr, ||);
-KOKKOS_IMPL_NEXTSILICON_PARALLEL_REDUCE_HELPER(BAnd, &);
-KOKKOS_IMPL_NEXTSILICON_PARALLEL_REDUCE_HELPER(BOr, |);
-
-#undef KOKKOS_IMPL_NEXTSILICON_PARALLEL_REDUCE_HELPER
+};
 
 #endif  // KOKKOS_NEXTSILICON_PARALLELREDUCE_RANGE_HPP
