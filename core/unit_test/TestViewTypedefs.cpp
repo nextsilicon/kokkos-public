b9a8d2a23426142e08d3ff4d702af048dfd88b77
Carl William Pearson <cwpears@sandia.gov>
2024-10-21 12:37:29 -0600

    next silicon has unique space accessibility

--- core/unit_test/TestViewTypedefs.cpp
+++ core/unit_test/TestViewTypedefs.cpp
@@ -178,8 +178,12 @@ constexpr bool test_view_typedefs(ViewParams<T, ViewArgs...>) {
                                  T, L, S, M, HostMirrorSpace, ValueType, ReferenceType>();
 }
 
-
+// FIXME_NEXTSILICON: NextSilicon basically acts like a host space, where host can access NextSiliconSpace and vis versa.
+#if defined(KOKKOS_ENABLE_NEXTSILICON)
+constexpr bool is_host_exec = true;
+#else
 constexpr bool is_host_exec = std::is_same_v<Kokkos::DefaultExecutionSpace, Kokkos::DefaultHostExecutionSpace>;
+#endif
 
 #if defined(KOKKOS_ENABLE_CUDA_UVM) || defined(KOKKOS_ENABLE_IMPL_CUDA_UNIFIED_MEMORY) || defined(KOKKOS_ENABLE_IMPL_HIP_UNIFIED_MEMORY)
 constexpr bool has_unified_mem_space = true;
