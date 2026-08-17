d1d01724a1c936e5aafeeab63b91e9df4110bfd9
Patrick Fasano <patrick.fasano@nextsilicon.com>
2026-08-17 15:51:37 -0500

    NextSilicon: switch from std::mutex to std::recursive_mutex
    Co-authored-by: Filip Parag <filip.parag@nextsilicon.com>
    Co-authored-by: Nemanja Grujic <nemanja.grujic@nextsilicon.com>
    Signed-off-by: Patrick Fasano <patrick.fasano@nextsilicon.com>

--- core/src/NextSilicon/Kokkos_NextSilicon_Instance.hpp
+++ core/src/NextSilicon/Kokkos_NextSilicon_Instance.hpp
@@ -20,7 +20,7 @@ namespace Kokkos::Experimental::Impl {
 
 class NextSiliconInternal {
   Impl::NextSiliconHeapBuffer functorBuffer_;
-  ::Kokkos::Impl::PageAlignedData<std::mutex,
+  ::Kokkos::Impl::PageAlignedData<std::recursive_mutex,
                                   ::Kokkos::Impl::PageLocation::Host>
       device_mutex_;
 
@@ -54,7 +54,7 @@ class NextSiliconInternal {
 
   uint32_t instance_id() const noexcept;
 
-  [[nodiscard]] std::lock_guard<std::mutex> lock_device();
+  [[nodiscard]] std::lock_guard<std::recursive_mutex> lock_device();
 };
 
 }  // namespace Kokkos::Experimental::Impl
